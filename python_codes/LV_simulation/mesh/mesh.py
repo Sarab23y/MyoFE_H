169323439c34fe67d680557006bcb161d33c2256
Sarab23y <sba431@uky.edu>
2025-03-13 16:44:07 -0400

    back

--- python_codes/LV_simulation/mesh/mesh.py
+++ python_codes/LV_simulation/mesh/mesh.py
@@ -7,10 +7,8 @@ Created on Mon Jan 10 11:15:59 2022
 from pyclbr import Function
 import numpy as np
 import json
-from dolfin import HDF5File
 from mpi4py import MPI
 from dolfin import *
-from dolfin import Mesh
 import os
 from ..dependencies.forms import Forms
 from ..dependencies.nsolver import NSolver#from ..dependencies.assign_heterogeneous_params import assign_heterogeneous_params as assign_params
@@ -31,37 +29,8 @@ class MeshClass():
 
         self.model = dict()
         self.data = dict()
+        
         if not predefined_mesh:
-            mesh_str = os.path.join(os.getcwd(), mesh_struct['mesh_path'][0])
-
-            self.model['mesh'] = Mesh()
-
-            # Handle file opening safely
-            if os.path.exists(mesh_str):
-                with HDF5File(mpi_comm_world(), mesh_str, 'r') as hdf5_file:
-                    datasets = hdf5_file.attributes("/")
-                    if "ellipsoidal" in datasets:
-                        hdf5_file.read(self.model['mesh'], "ellipsoidal", False)
-                    else:
-                        # Handle missing dataset: assign a default or empty mesh
-                        self.model['mesh'] = Mesh()  # Fallback to empty mesh
-            else:
-                self.model['mesh'] = Mesh()  # Fallback in case of missing file
-
-        else:
-            self.model['mesh'] = predefined_mesh
-
-        # Parallel communicator
-        self.comm = self.model['mesh'].mpi_comm()
-
-        # Safe handling for MeshFunction
-        try:
-            subdomains = MeshFunction('int', self.model['mesh'], 3)
-            self.no_of_cells = len(subdomains.array())
-        except:
-            self.no_of_cells = 0 
-
-        """if not predefined_mesh:
             mesh_str = os.path.join(os.getcwd(),mesh_struct['mesh_path'][0])
         
             self.model['mesh'] = Mesh()
@@ -84,7 +53,6 @@ class MeshClass():
         self.no_of_cells = len(subdomains.array())
         #print "no of cells"
         #print self.no_of_cells
-"""
         
 
         self.model['function_spaces'] = self.initialize_function_spaces(mesh_struct)
